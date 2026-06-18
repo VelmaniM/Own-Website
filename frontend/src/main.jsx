ff85ead4b811e37b312c035ed58d4e071389a236
Velmanikandan <velmanikandan@Velmanikandans-MacBook-Air.local>
2026-06-18 21:05:54 +0530

    Add Vercel Speed Insights

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,9 +7,11 @@ import '@fontsource/inter/600.css';
 import '@fontsource/inter/700.css';
 import './index.css'
 import App from './App.jsx'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <SpeedInsights />
   </StrictMode>,
 )
